2f11dbbe5c803b0981a52355f3d019d0e4e3fd5a
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-06-04 19:03:37 +0100

    quiz app

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+
+export default function App() {
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+	const [currentQuestion , setCurrentQuestion] = useState(0)
+	const [showScore, setShowScore] = useState(false)
+	const [score, setScore] = useState(0)
+	const [formerScore , setFormerScore] = useState('')
+
+	const handleAnswerButtonClick = (isCorrect) =>{
+        if(isCorrect === true){
+			setScore(score + 1)
+		}
+
+		const nextQuestion = currentQuestion + 1
+		if(nextQuestion < questions.length){
+			setCurrentQuestion(nextQuestion)
+		} else {
+			setShowScore(true)
+		}
+	}
+
+	const rePlay = () =>{
+		setShowScore(false)
+		setCurrentQuestion(0)
+		setScore(0)
+		setFormerScore(score)
+	}
+
+	return (
+		<div className='app'>
+			{/* HINT: replace "false" with logic to display the 
+      score when the user has answered all the questions */}
+			{showScore ? (
+				<div className='score-section'>
+					{formerScore !== 0  ? <div>{`Your former score is ${formerScore}`}</div>: 'Nice try you can try do better' }
+					<br />
+					You scored {score} out of {questions.length}
+					<button onClick={rePlay}>Retake</button>
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map(({answerText,isCorrect})=>{
+							return(
+								<button onClick={()=>handleAnswerButtonClick(isCorrect)}>{answerText}</button>
+							)
+						})}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
